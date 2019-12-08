2e5139e6e4a93f437a82c8f53e69df706824e8f1
76 <why771102@gmail.com>
2019-12-08 15:53:36 +0800

    no message

---
+++ ProjectMarry/src/main/java/com/Servlet/UserLogOut.java
@@ -0,0 +1,81 @@
+package com.Servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import com.Bean.UserBean;
+import com.Dao.UserDaoImpl;
+
+
+@WebServlet("/UserLogOut")
+public class UserLogOut extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    Connection conn;
+
+    public UserLogOut() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		init();
+		HttpSession session;
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("txex/html;charset=UTF-8");
+		
+		
+			Cookie[] cookie =  request.getCookies();
+			
+			for(int i = 0; i > cookie.length; i++) {
+				System.out.println(cookie[i]);
+			}
+			
+			
+		
+
+			
+		
+
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	
+	public void init() {
+		
+		Context context;
+		try {
+			context = new InitialContext();
+			DataSource ds = (DataSource)context.lookup("java:/comp/env/jdbc/servdb");
+			 try {
+				conn = ds.getConnection();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+}
