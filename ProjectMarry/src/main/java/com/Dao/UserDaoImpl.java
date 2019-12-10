ebc058c6f2d6c0fa42b3b2de22f214911c114010
76 <why771102@gmail.com>
2019-12-10 16:24:32 +0800

    no message

--- ProjectMarry/src/main/java/com/Dao/UserDaoImpl.java
+++ ProjectMarry/src/main/java/com/Dao/UserDaoImpl.java
@@ -22,8 +22,18 @@ public class UserDaoImpl implements IUserDao {
 		String sqlstmt = "insert into Members (Name, Account,Pwd,Birth,Gender,Mobile,Tel,UID,Mail,Address) values (?,?,?,?,?,?,?,?,?,?)";
 		
 		try {
-			
+            
 			PreparedStatement rs = conn.prepareStatement(sqlstmt);
+			rs.setString(1, u.getName());
+            rs.setString(2, u.getAccount());
+            rs.setString(3, u.getPwd());
+            rs.setString(4, u.getBirth());
+            rs.setString(5, u.getGender());
+            rs.setString(6, u.getMobile());
+            rs.setString(7, u.getTel());
+            rs.setString(8, u.getUid());
+            rs.setString(9, u.getMail());
+            rs.setString(10, u.getAddress());
 			int status = rs.executeUpdate();
 			
 			return u;
