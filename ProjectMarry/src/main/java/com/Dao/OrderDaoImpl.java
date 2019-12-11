df8e4452834d434b52d3ebe250b5de06f2f1c745
Genie <genie.sheu@gms.tku.edu.tw>
2019-12-11 14:47:00 +0800

    no message

---
+++ ProjectMarry/src/main/java/com/Dao/OrderDaoImpl.java
@@ -0,0 +1,74 @@
+package com.Dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.List;
+
+import com.Bean.OrderBean;
+import com.Interface.IOrderDao;
+
+public class OrderDaoImpl implements IOrderDao{
+	private Connection con;
+	int orderNo = 0;
+	@Override
+	public void insertOrder(OrderBean ob) {
+		String sqlOrder = "Insert Into Order"
+				+ "(ID, StartDate, EndDate, InvoiceTitle, "
+				+ "VATnumber, ShippingAddress) values(?, ?, ?, ?, ?, ?)";
+		String sqlOrderDetails = "Insert Into OrderDetails"
+				+ "(OrderID, ProductID, ProductName, Quantity, "
+				+ "UnitPrice, SubTotal, Discount, OrderDate, Memo, "
+				+ "ShipmentStatus) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		ResultSet generatedKeys = null;
+		
+		try {
+			PreparedStatement stmt = 
+					con.prepareStatement(sqlOrder, Statement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1, ob.getID());
+			Timestamp tsSD = new Timestamp(ob.getStartDate().getDate());
+			stmt.setTimestamp(2, tsSD);
+			Timestamp tsED = new Timestamp(ob.getEndDate().getDate());
+			stmt.setTimestamp(3, tsED);
+			stmt.setString(4, ob.getInvoiceTitle());
+			stmt.setString(5, ob.getVATnumber());
+			stmt.setString(6, ob.getShippingAddress());
+			stmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	@Override
+	public void setConnection(Connection con) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public OrderBean getOrder(int orderNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderBean> getAllOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderBean> getMemberOrders(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
