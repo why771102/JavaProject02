95adc48e3d8f5d5798fe44da9e12778c5ebe0351
Mary <uniwanling@gmail.com>
2019-12-24 18:37:10 +0800

    no message

--- ProjectMarry/src/main/java/com/Servlet/UploadFiles.java
+++ ProjectMarry/src/main/java/com/Servlet/UploadFiles.java
@@ -1,6 +1,8 @@
 package com.Servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -16,6 +18,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import javax.sql.DataSource;
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialException;
 
 import com.Service.VenueImageService;
 import com.Service.VenueImageServiceImpl;
@@ -39,27 +43,52 @@ public class UploadFiles extends HttpServlet {
 		init();
 		System.out.println("000000");
 		VenueImageService vis = new VenueImageServiceImpl(conn);
+		
+		Part part = request.getPart("photo1");
+		InputStream in = part.getInputStream();
+		String header = part.getHeader("Content-Disposition");
+		System.out.println(header);
+		// header=> form-data; name="photo"; filename="all.png" -- chrome, firefox
+		// use indexOf to catch "filename" position
+		int idx = header.indexOf("filename");
+		System.out.println(idx);
+		// idx=> position 25 will be the start of "filename"
+		// use substring to extract the position of filename => all.png
+		String filename = header.substring(idx + 10, header.length() - 1);
 
-		String savePath = this.getServletContext().getRealPath("Files");
+		byte[] buf = new byte[1024]; //1024 memory given here
+		int length;
+		while ((length = in.read(buf)) != -1) {
+			try {
+				Blob blob = new SerialBlob(buf);
+				vis.processInsertImage(blob, "V1", filename);
+			} catch (SerialException e) {
 
-		List<Part> photos = (List<Part>) request.getParts();
-		for (Part photo : photos) {
-			String header = photo.getHeader("Content-Dispostion");
-			int start = header.lastIndexOf("=");
-			String fileName = header.substring(start + 1).replace("\"", "");
-			
-			if(fileName != null && !"".equals(fileName)) {
-				photo.write(savePath + "/" + fileName);
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-			vis.processInsertImage(photo, "V1", fileName);
 		}
+//		String savePath = this.getServletContext().getRealPath("Files");
+
+//		List<Byte> photos = (List<Byte>)request.getInputStream();
+//		for (Byte photo : photos) {
+//			String header = photo.getHeader("Content-Dispostion");
+//			int start = header.lastIndexOf("=");
+//			String fileName = header.substring(start + 1).replace("\"", "");
+//			
+//			if(fileName != null && !"".equals(fileName)) {
+//				photo.write(savePath + "/" + fileName);
+//			}
+
+//			vis.processInsertImage(photo, "V1", fileName);
+//		}
 		
 		response.getWriter().append("Insert Successful!!").append(request.getContextPath());
 
 		try {
 			conn.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
